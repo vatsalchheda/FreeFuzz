4c3f3fcae0a3c0829c2aff7a29f13c2fb1e71dcc
vchheda2 <49812773+vatsalchheda@users.noreply.github.com>
2023-03-30 18:37:45 -0500

    Updated error files and instrumentation

--- Scrape/run_documentation.py
+++ Scrape/run_documentation.py
@@ -34,11 +34,6 @@
         
 
 import csv
-
-# note: If you use 'b' for the mode, you will get a TypeError
-# under Python3. You can just use 'w' for Python 3
-
-
 import os
 import subprocess
 
@@ -50,13 +45,12 @@ file_list = os.listdir(dir_path)
 
 # create an empty list to store the names of the files that produced an error
 error_files = []
-lines_list = open("Scrape/error_documentation_wo_hijack.txt").read().splitlines()
-# print(lines_list)
+
 # iterate over each file in the list
 for file_name in file_list:
 
     # check if the file is a Python file
-    if file_name in lines_list and file_name.endswith('.py') and not file_name.startswith('paddle_fluid'):
+    if file_name.endswith('.py') and not file_name.startswith('paddle_fluid'):
 
         # build the command to run the file using the Python interpreter
         command = f'python {os.path.join(dir_path, file_name)}'
@@ -68,20 +62,23 @@ for file_name in file_list:
         if result.returncode != 0:
             error_files.append((file_name,result.stderr.decode()))
             print(f"{file_name} produced an error:")
-            print(result.stderr.decode())
+            # print(result.stderr.decode())
 
-with open('error_1.txt', 'w') as fp:
+with open('Scrape/Errors/errors_wo_Hijack.txt', 'w') as fp:
     fp.write('\n'.join('%s %s' % x for x in error_files))
 # print the list of error files
 
+# Open a CSV file for writing
+with open('Scrape/Errors/errors_wo_Hijack.csv', 'w', newline='') as file:
+    # Create a CSV writer object
+    writer = csv.writer(file)
 
+    # Write the headers (optional)
+    writer.writerow(['API', 'Error'])
 
-with open('errors.csv','wb') as out:
-    # csv_out=csv.writer(out)
-    # csv_out.writerow(['name','num'])
-    # for row in error_files:
-    #     csv_out.writerow(row)
-    out.write('\n'.join('%s , %s' % x for x in error_files))
+    # Write the data rows
+    for row in error_files:
+        writer.writerow(row)
 
 if error_files:
     print('The following files produced an error:')
